1f49b008ebda49ca403efbc9070959e2ae680b2d
garlic0x1 <garlic1@protonmail.com>
2022-12-27 03:54:54 -0600

    fc

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use anyhow::Result;
+use clap::Parser;
+use serde::Deserialize;
+use std::process::Command;
+use std::{collections::HashMap, thread, time};
+use walkdir;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Arguments {
+    #[clap(default_value = ".dwatch", short, long)]
+    file: String,
+
+    #[clap(default_value_t = 2, short, long)]
+    delay: u64,
+}
+
+#[derive(Deserialize)]
+struct JobConfig {
+    dir: String,
+    script: String,
+}
+
+struct Job {
+    dir: String,
+    script: String,
+    history: HashMap<String, time::SystemTime>,
+}
+
+fn updated(job: &mut Job) -> bool {
+    0 < walkdir::WalkDir::new(job.dir.clone())
+        .into_iter()
+        .filter_map(|file| file.ok())
+        .map(|file| -> Result<bool> {
+            let modified = file.metadata()?.modified()?;
+            let filename = file.path().to_str().unwrap_or(".").to_string();
+            if let Some(last) = job.history.insert(filename.clone(), modified) {
+                Ok(last != modified)
+            } else {
+                Ok(true)
+            }
+        })
+        .filter_map(|res| res.ok())
+        .filter(|res| *res)
+        .count()
+}
+
+fn do_job(job: &mut Job) {
+    if updated(job) {
+        println!("running: {}", &job.script);
+        let out = Command::new("sh")
+            .arg("-c")
+            .arg(&job.script)
+            .output()
+            .expect("failed to execute process");
+        println!("finished job: {:?}", out);
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Arguments::parse();
+    let jobs_config: Vec<JobConfig> = serde_json::from_str(&std::fs::read_to_string(args.file)?)?;
+    let mut jobs: Vec<Job> = jobs_config
+        .iter()
+        .map(|config| Job {
+            dir: config.dir.clone(),
+            script: config.script.clone(),
+            history: HashMap::new(),
+        })
+        .collect();
+
+    loop {
+        thread::sleep(time::Duration::from_secs(args.delay));
+        for i in 0..jobs.len() {
+            do_job(&mut jobs[i]);
+        }
+
+        if false {
+            break;
+        }
+    }
+
+    Ok(())
+}
