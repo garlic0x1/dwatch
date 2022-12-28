54857ad5f92fb5ec6a034b9fd91a57b82298fc95
garlic0x1 <garlic1@protonmail.com>
2022-12-28 01:59:37 -0600

    hi

---
+++ src/job.rs
@@ -0,0 +1,110 @@
+use anyhow::Result;
+use std::collections::HashMap;
+use std::ffi::OsStr;
+use std::path::Path;
+use std::process::{Child, Command};
+use std::{thread, time};
+use walkdir;
+
+use crate::JobConfig;
+
+pub struct Job {
+    dir: String,
+    filetypes: Option<Vec<String>>,
+    scripts: Vec<String>,
+    servers: Vec<String>,
+    delay: u64,
+    processes: HashMap<String, Child>,
+    history: HashMap<String, time::SystemTime>,
+}
+
+impl Job {
+    pub fn from_config(cfg: JobConfig) -> Self {
+        Self {
+            dir: cfg.dir,
+            filetypes: cfg.filetypes,
+            scripts: cfg.scripts.unwrap_or(vec![]),
+            servers: cfg.servers.unwrap_or(vec![]),
+            delay: cfg.delay.unwrap_or(2),
+            processes: HashMap::new(),
+            history: HashMap::new(),
+        }
+    }
+
+    pub async fn watch(&mut self) {
+        loop {
+            println!("checking");
+            if self.updated() {
+                println!("changed");
+                self.run_scripts();
+                self.run_servers();
+            }
+            thread::sleep(time::Duration::from_secs(self.delay));
+        }
+    }
+
+    fn run_servers(&mut self) {
+        self.servers.iter().for_each(|server| {
+            if let Some(proc) = self.processes.get_mut(server) {
+                println!("killing: '{}'", server);
+                proc.kill().expect("failed to kill child");
+            }
+
+            println!("restarting: '{}'", server);
+            let child = Command::new("sh")
+                .arg("-c")
+                .arg(server)
+                .spawn()
+                .expect("failed to spawn process");
+
+            self.processes.insert(server.clone(), child);
+        })
+    }
+
+    fn run_scripts(&self) {
+        println!("{:?}", self.scripts);
+        self.scripts.iter().for_each(|script| {
+            println!("running: '{}'", script);
+            let out = Command::new("sh")
+                .arg("-c")
+                .arg(script)
+                .output()
+                .expect("failed to execute process");
+            let stdout = String::from_utf8(out.stdout).unwrap();
+            let stderr = String::from_utf8(out.stderr).unwrap();
+
+            println!("STDOUT:\n{}", stdout);
+            if stderr != "" {
+                println!("STDERR:\n{}", stderr);
+            }
+        });
+    }
+
+    fn updated(&mut self) -> bool {
+        0 < walkdir::WalkDir::new(self.dir.clone())
+            .into_iter()
+            .filter_map(|file| file.ok())
+            // check filetype
+            .filter(|file| {
+                let ext = Path::new(file.file_name())
+                    .extension()
+                    .and_then(OsStr::to_str)
+                    .unwrap_or_default()
+                    .to_string();
+                self.filetypes.is_none() || self.filetypes.as_ref().unwrap().contains(&ext)
+            })
+            // check modified
+            .map(|file| -> Result<bool> {
+                let modified = file.metadata()?.modified()?;
+                let filename = file.path().to_str().unwrap_or(".").to_string();
+                if let Some(last) = self.history.insert(filename.clone(), modified) {
+                    Ok(last != modified)
+                } else {
+                    Ok(true)
+                }
+            })
+            .filter_map(|res| res.ok())
+            .filter(|res| *res)
+            .count()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+mod job;
+
 use anyhow::Result;
 use clap::Parser;
+use futures::future::join_all;
+use job::Job;
 use serde::Deserialize;
-use std::process::Command;
-use std::{collections::HashMap, thread, time};
-use walkdir;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -11,72 +12,27 @@ use walkdir;
 struct Arguments {
     #[clap(default_value = ".dwatch", short, long)]
     file: String,
-
-    #[clap(default_value_t = 2, short, long)]
-    delay: u64,
 }
 
-#[derive(Deserialize)]
-struct JobConfig {
+#[derive(Deserialize, Clone)]
+pub struct JobConfig {
     dir: String,
-    script: String,
+    filetypes: Option<Vec<String>>,
+    scripts: Option<Vec<String>>,
+    servers: Option<Vec<String>>,
+    delay: Option<u64>,
 }
 
-struct Job {
-    dir: String,
-    script: String,
-    history: HashMap<String, time::SystemTime>,
-}
-
-fn updated(job: &mut Job) -> bool {
-    0 < walkdir::WalkDir::new(job.dir.clone())
-        .into_iter()
-        .filter_map(|file| file.ok())
-        .map(|file| -> Result<bool> {
-            let modified = file.metadata()?.modified()?;
-            let filename = file.path().to_str().unwrap_or(".").to_string();
-            if let Some(last) = job.history.insert(filename.clone(), modified) {
-                Ok(last != modified)
-            } else {
-                Ok(true)
-            }
-        })
-        .filter_map(|res| res.ok())
-        .filter(|res| *res)
-        .count()
-}
-
-fn do_job(job: &mut Job) {
-    if updated(job) {
-        println!("running: '{}'", &job.script);
-        let out = Command::new("sh")
-            .arg("-c")
-            .arg(&job.script)
-            .output()
-            .expect("failed to execute process");
-        let stdout = String::from_utf8(out.stdout).unwrap();
-        let stderr = String::from_utf8(out.stderr).unwrap();
-        println!("STDOUT:\n{}", stdout);
-        if stderr != "" {
-            println!("STDERR:\n{}", stderr);
-        }
-    }
-}
-
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Arguments::parse();
     let jobs_config: Vec<JobConfig> = serde_yaml::from_str(&std::fs::read_to_string(args.file)?)?;
-    let mut jobs: Vec<Job> = jobs_config
+    let mut jobs = jobs_config
         .iter()
-        .map(|config| Job {
-            dir: config.dir.clone(),
-            script: config.script.clone(),
-            history: HashMap::new(),
-        })
-        .collect();
+        .map(|config| Job::from_config(config.clone()))
+        .collect::<Vec<Job>>();
 
-    loop {
-        thread::sleep(time::Duration::from_secs(args.delay));
-        (0..jobs.len()).for_each(|i| do_job(&mut jobs[i]));
-    }
+    join_all(jobs.iter_mut().map(|job| job.watch()).collect::<Vec<_>>()).await;
+
+    Ok(())
 }
